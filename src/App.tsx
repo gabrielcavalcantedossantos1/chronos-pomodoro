ea6382b83f2dcbc99710256618b5895deafeb042
Gabrie Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-01-28 07:39:21 -0300

    envio do CountDown

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Heading from './components/Heading'
 import Container from './components/Container'
 import Logo from './components/Logo'
 import Menu from './components/Menu'
+import CountDown from './components/CountDown'
 
 //icons
 //import {TimerIcon } from 'lucide-react'
@@ -21,6 +22,10 @@ const App = () => {
 
   <Container>
     <Menu/>
+  </Container>
+
+  <Container>
+    <CountDown/>
   </Container>
   </>
   )
---
+++ src/components/CountDown/index.tsx
@@ -0,0 +1,13 @@
+//css
+import styles from './styles.module.css'
+
+
+const CountDown = () => {
+ return (
+    <div className={styles.container}>
+        00:00
+    </div>
+  )
+}
+
+export default CountDown
