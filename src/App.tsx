c9dfc7eb95148a47158f22828ad9d386ec7c16d7
Gabrie Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-01-31 14:13:15 -0300

    criando as config do objeto das tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 //components
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider'
-import { useTaskContext } from './contexts/TaskContext/useTaskContext'
 import Home from './pages/Home'
 
 //css
@@ -9,10 +8,6 @@ import './styles/thema.css'
 
 
 const App = () => {
-
-  //teste de context
-  const {state} = useTaskContext()
-  console.log(state)
   return (
     <TaskContextProvider>
       <Home />
--- src/components/mainForm/Index.tsx
+++ src/components/mainForm/Index.tsx
@@ -3,20 +3,69 @@ import DefaultInput from '../DefaultInput/Index'
 import DefaultButton from '../DefaultButton/Index'
 import Cycles from '../Cycles/Index'
 import {PlayCircleIcon} from 'lucide-react'
-import { useTaskContext } from '../../contexts/TaskContext/useTaskContext'
+//import { useTaskContext } from '../../contexts/TaskContext/useTaskContext'
+import { useRef } from 'react'
+import type { TaskModel } from '../../models/TaskModel'
+import { useTaskContext } from '../../contexts/TaskContext'
+import { getNextCycle } from '../../utils/getNextCycle'
+import { getNextCycleType } from '../../utils/getNextCycleType'
 
 
 const MainForm = () => {
-  const { setState } = useTaskContext()
-  console.log(setState)
+  const { state, setState} = useTaskContext()
+  //const [taskName, setTaskName] = useState('')
+  const taskNameInput = useRef<HTMLInputElement>(null)
+
+  //ciclos
+  const nextCycle = getNextCycle(state.currentCycle)
+  const nextCycleType = getNextCycleType(nextCycle)
+
+  function handleCreateNewTask(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    
+    if(taskNameInput.current === null) return
+
+    const taskName = taskNameInput.current.value.trim()
+    
+    if(!taskName) {
+      alert('Digite algo')
+      return
+    }
+    
+    const newTask: TaskModel = {
+      id: (Math.random() * 100).toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: nextCycleType
+    }
+
+    const secondsRemaining = newTask.duration * 60
+
+    setState(state => ({
+      ...state,
+      config: {...state.config},
+      activeTask:newTask,
+      currentCycle: nextCycle,
+      secondsRemaining, //coferir
+      formattedSecondsRemaining: '00:00',
+      tasks: [...state.tasks, newTask]
+    }))
+  }
+
   return (
-  <form action="" className='form'>
+  <form onSubmit={handleCreateNewTask} action="" className='form'>
       <div className="formRow">
         <DefaultInput 
         id='input' 
         type='text' 
         label='Tarefa'
         placeholder='Digite algo'
+        // value={taskName}
+        // onChange={e => setTaskName(e.target.value)}
+        ref={taskNameInput}
         />
       </div>
 
