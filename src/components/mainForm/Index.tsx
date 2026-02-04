620a0a84323d6e7df6ae5f70c7b6d961eeba3af0
Gabrie Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-02-04 12:58:12 -0300

    troca dos ciclos no projeto

--- src/components/mainForm/Index.tsx
+++ src/components/mainForm/Index.tsx
@@ -1,103 +1,109 @@
-//components
+// React
+import { useRef } from 'react'
+
+// Libs
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react'
+
+// Components
 import DefaultInput from '../DefaultInput/Index'
 import DefaultButton from '../DefaultButton/Index'
 import Cycles from '../Cycles/Index'
-import {PlayCircleIcon, StopCircleIcon} from 'lucide-react'
-//import { useTaskContext } from '../../contexts/TaskContext/useTaskContext'
-import { useRef } from 'react'
-import type { TaskModel } from '../../models/TaskModel'
+import { Tips } from '../tips'
+
+// Context
 import { useTaskContext } from '../../contexts/TaskContext'
-import { getNextCycle } from '../../utils/getNextCycle'
-import { getNextCycleType } from '../../utils/getNextCycleType'
 import { TaskActionType } from '../../contexts/TaskContext/taskActions'
 
+// Utils
+import { getNextCycle } from '../../utils/getNextCycle'
+import { getNextCycleType } from '../../utils/getNextCycleType'
+
+// Types
+import type { TaskModel } from '../../models/TaskModel'
 
 const MainForm = () => {
-  const { state, dispatch} = useTaskContext()
+  const { state, dispatch } = useTaskContext()
   const taskNameInput = useRef<HTMLInputElement>(null)
 
-  //ciclos
+  // Ciclos
   const nextCycle = getNextCycle(state.currentCycle)
   const nextCycleType = getNextCycleType(nextCycle)
 
   function handleCreateNewTask(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
-    
-    if(taskNameInput.current === null) return
+
+    if (!taskNameInput.current) return
 
     const taskName = taskNameInput.current.value.trim()
 
-
-    
-    if(!taskName) {
+    if (!taskName) {
       alert('Digite algo')
       return
     }
-    
+
     const newTask: TaskModel = {
-      id: Math.random().toString(),
+      id: Date.now().toString(),
       name: taskName,
       startDate: Date.now(),
       completeDate: null,
       interruptDate: null,
       duration: state.config[nextCycleType],
-      type: nextCycleType
+      type: nextCycleType,
     }
 
-
-    dispatch({type: TaskActionType.START_TASK, payload :newTask})
+    dispatch({
+      type: TaskActionType.START_TASK,
+      payload: newTask,
+    })
   }
 
   function handleInterruptTask() {
-    dispatch({type: TaskActionType.INTERRUPT_TASK})
-
+    dispatch({ type: TaskActionType.INTERRUPT_TASK })
   }
 
   return (
-  <form onSubmit={handleCreateNewTask} action="" className='form'>
+    <form onSubmit={handleCreateNewTask} className="form">
       <div className="formRow">
-        <DefaultInput 
-        id='input' 
-        type='text' 
-        label='Tarefa'
-        placeholder='Digite algo'
-        ref={taskNameInput}
-        disabled={!!state.activeTask}
+        <DefaultInput
+          id="input"
+          type="text"
+          label="Tarefa"
+          placeholder="Digite algo"
+          ref={taskNameInput}
+          disabled={!!state.activeTask}
         />
       </div>
 
       <div className="formRow">
-        <p>Próximo ciclo em 25 min</p>
+        <Tips />
       </div>
 
-      {state.currentCycle > 0
-       && (
+      {state.currentCycle > 0 && (
         <div className="formRow">
-          <Cycles/>
+          <Cycles />
         </div>
       )}
 
       <div className="formRow">
         {!state.activeTask ? (
-          <DefaultButton 
-            aria-label='Iniciar' 
-            title='Iniciar' 
-            icon={<PlayCircleIcon/>} 
-            type='submit' 
-            key='botao_submit'
+          <DefaultButton
+            key="botao_submit"
+            aria-label="Iniciar"
+            title="Iniciar"
+            icon={<PlayCircleIcon />}
+            type="submit"
           />
         ) : (
-          <DefaultButton 
-            aria-label='Parar' 
-            title='parar' 
-            icon={<StopCircleIcon/>} 
-            type='button' 
-            color='red' 
-            onClick={handleInterruptTask} 
-            key='botao_button'
+          <DefaultButton
+            key="botao_button"
+            aria-label="Parar"
+            title="Parar"
+            icon={<StopCircleIcon />}
+            type="button"
+            color="red"
+            onClick={handleInterruptTask}
           />
         )}
-          
       </div>
     </form>
   )
