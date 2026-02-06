1e1d48a800aea41587847f7f584aba78f2dbd54c
Gabrie Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-02-05 22:18:54 -0300

    aúdio ao finalizar a tarefa

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer } from "react"
+import { useEffect, useReducer, useRef } from "react"
 import { initialTaskState } from "./initialTaskState"
 import { TaskContext } from "./TaskContext"
 import { taskReducer } from "./taskReducert"
 import { TimerWorkerManager } from "../../workers/TimerWorkerManager"
 import { TaskActionType } from "./taskActions"
+import { loadBeep } from "../../utils/loadBeep"
 
 type TaskContextProviderProps = {
   children: React.ReactNode
@@ -12,39 +13,50 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState)
 
+  const playBeepRef = useRef<(() => void) | null>(null)
+
   useEffect(() => {
+    if (state.activeTask && !playBeepRef.current) {
+      playBeepRef.current = loadBeep()
+    }
+
+    if (!state.activeTask) {
+      playBeepRef.current = null
+    }
+  }, [state.activeTask])
+
+  useEffect(() => {
+
+    if (!state.activeTask) return
+
     const worker = TimerWorkerManager.getInstance()
 
     const handleMessage = (e: MessageEvent) => {
       const secondsRemaining = e.data as number
 
-      if (!state.activeTask) {
-        worker.terminate()
-        return
-      }
-
       if (secondsRemaining <= 0) {
+        playBeepRef.current?.()
+        playBeepRef.current = null
+
         dispatch({ type: TaskActionType.COMPLETE_TASK })
         worker.terminate()
         return
       }
 
+      
       dispatch({
         type: TaskActionType.COUNT_DOWN,
         payload: { secondsRemaining }
       })
     }
 
-    if (!state.activeTask) {
-      worker.terminate()
-      return
-    }
-
     worker.addEventListener("message", handleMessage)
+
     worker.postMessage(state)
 
     return () => {
       worker.removeEventListener("message", handleMessage)
+      worker.terminate()
     }
   }, [state.activeTask])
 
