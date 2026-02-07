a435665e9d08aa2d2ffcf211cc1c016c55ae5a69
Gabrie Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-02-07 10:13:26 -0300

    Limpando o App.tsx e criando um componente para as rotas do React-Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
 //components
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider'
-import Home from './pages/Home'
 
 //css
 import './styles/global.css'
 import './styles/thema.css'
 import { MessagesContainer } from './components/messagesContainer'
-import { BrowserRouter, Routes , Route} from 'react-router'
-import NotFound from './pages/NotFound'
-import AboutPomodoro from './pages/AboutPomodoro'
+import { MainRouter } from './routers/MainRouter'
 
 
 const App = () => {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/about-pomodoro' element={<AboutPomodoro/>}/>
-
-            <Route path='*' element={<NotFound/>}/>
-          </Routes>
-        </BrowserRouter>
+        <MainRouter/>
       </MessagesContainer>
     </TaskContextProvider>
   )
