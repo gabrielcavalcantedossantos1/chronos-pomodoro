ae3bdc40207b6b6ea0c954d41694eb66492ba030
Gabrie Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-02-07 14:16:04 -0300

    Enviando as tarefas para o localStorage

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -5,23 +5,47 @@ import { taskReducer } from "./taskReducert"
 import { TimerWorkerManager } from "../../workers/TimerWorkerManager"
 import { TaskActionType } from "./taskActions"
 import { loadBeep } from "../../utils/loadBeep"
+import type { TaskStateModel } from "../../models/TaskstateModel"
 
 type TaskContextProviderProps = {
   children: React.ReactNode
 }
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
-  const [state, dispatch] = useReducer(taskReducer, initialTaskState)
+  const [state, dispatch] = useReducer(
+  taskReducer,
+  initialTaskState,
+  () => {
+    const storageState = localStorage.getItem('state')
+
+    if (!storageState) {
+      return initialTaskState
+    }
+
+    const parsedStorageState = JSON.parse(storageState) as TaskStateModel
+
+    return {
+      ...parsedStorageState,
+      activeTask:null,
+      secondsRemaining: 0,
+      formattedSecondsRemaining: '00:00'
+    }
+  }
+)
+
 
   const playBeepRef = useRef<(() => void) | null>(null)
 
   useEffect(() => {
+    
+        localStorage.setItem('tasks', JSON.stringify(state))
 
     document.title = `${state.formattedSecondsRemaining} - Chronos Pomodoro `
 
   }, [Worker,state])
 
   useEffect(() => {
+
     if (state.activeTask && !playBeepRef.current) {
       playBeepRef.current = loadBeep()
     }
